aefc78e68b2874171f10fc19e22c7f4adb35e172
Diego Valenzuela <valenv.dg@gmail.com>
2020-02-02 23:12:54 -0600

    Added UI elements

--- src/FWindow.cpp
+++ src/FWindow.cpp
@@ -1,8 +1,18 @@
 #include "FElement.h"
 #include "FWindow.h"
 #include <vector>
+#include <cstring>
+#include <string>
+
+#define INITIAL_FILE_Y_POSITION 70
+#define INITIAL_FILE_X_POSITION 20
 
 std::vector<XRectangle> files;
+std::vector<XRectangle> ui_elems;
+std::vector<BitmapData> bitmaps;
+
+std::string WINDOW_TITLE = "Sistemas Operativos - File Explorer";
+std::string current_path = "/path/to/current/dir";
 
 FWindow::FWindow()
 {
@@ -36,19 +46,22 @@ void FWindow::init_window()
     /* select kind of events we are interested in */
     XSelectInput(d, w, ExposureMask | KeyPressMask);
 
-    set_window_title("Sistemas Operativos - File Explorer");
+    set_window_title(&WINDOW_TITLE[0]);
+    ui_elems.push_back(create_rectangle(0, 45, 800, 5));
+    bitmaps.push_back(createBitmap("/media/diego/Secondary_ext41/Universidad/Sistemas_Operativos_II/File_Explorer/img/folder.xbm"));
+
 
     /* Rectangles for testing */
-    files.push_back(create_rectangle(20, 20, 100, 100));
-    files.push_back(create_rectangle(170, 20, 100, 100));
-    files.push_back(create_rectangle(320, 20, 100, 100));
-    files.push_back(create_rectangle(470, 20, 100, 100));
-    files.push_back(create_rectangle(620, 20, 100, 100));
-    files.push_back(create_rectangle(20, 170, 100, 100));
-    files.push_back(create_rectangle(170, 170, 100, 100));
-    files.push_back(create_rectangle(320, 170, 100, 100));
-    files.push_back(create_rectangle(470, 170, 100, 100));
-    files.push_back(create_rectangle(620, 170, 100, 100));
+    files.push_back(create_rectangle(20, 70, 100, 100));
+    files.push_back(create_rectangle(170, 70, 100, 100));
+    files.push_back(create_rectangle(320, 70, 100, 100));
+    files.push_back(create_rectangle(470, 70, 100, 100));
+    files.push_back(create_rectangle(620, 70, 100, 100));
+    files.push_back(create_rectangle(20, 220, 100, 100));
+    files.push_back(create_rectangle(170, 220, 100, 100));
+    files.push_back(create_rectangle(320, 220, 100, 100));
+    files.push_back(create_rectangle(470, 220, 100, 100));
+    files.push_back(create_rectangle(620, 220, 100, 100));
 
     /* map (show) the window */
     XMapWindow(d, w);
@@ -62,9 +75,11 @@ void FWindow::open_window()
         /* draw or redraw the window */
         if (e.type == Expose)
         {
-            // Rectangle gets drawn on screen
-            /* XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10); */
+            // Draw elements on screen
+            XDrawString(d, w, DefaultGC(d, s), 300, 28, &current_path[0], current_path.size());
+            XFillRectangles(d, w, DefaultGC(d, s), &ui_elems[0], ui_elems.size());
             XFillRectangles(d, w, DefaultGC(d, s), &files[0], files.size());
+            for(auto bmps : bitmaps) XCopyPlane(d, bmps.P, w, DefaultGC(d, s), 0, 0, bmps.width, bmps.height, 100, 5, 1);
         }
         /* exit on key press */
         if (e.type == KeyPress)
@@ -98,4 +113,63 @@ XRectangle FWindow::create_rectangle(int16_t x, int16_t y, uint16_t width, uint1
     XR.width = width;
     XR.height = height;
     return XR;
+}
+
+
+BitmapData FWindow::createBitmap(std::string filename)
+{
+    /* this variable will contain the ID of the newly created pixmap.    */
+    Pixmap bitmap;
+    BitmapData BD;
+
+    /* these variables will contain the dimensions of the loaded bitmap. */
+    unsigned int bitmap_width, bitmap_height;
+
+    /* these variables will contain the location of the hot-spot of the   */
+    /* loaded bitmap.                                                    */
+    int hotspot_x, hotspot_y;
+
+    /* this variable will contain the ID of the root window of the screen */
+    /* for which we want the pixmap to be created.                        */
+    Window root_win = DefaultRootWindow(d);
+
+    /* load the bitmap found in the file "icon.bmp", create a pixmap      */
+    /* containing its data in the server, and put its ID in the 'bitmap'  */
+    /* variable.                                                          */
+    int rc = XReadBitmapFile(d, root_win,
+                            filename.c_str(),
+                            &bitmap_width, &bitmap_height,
+                            &bitmap,
+                            &hotspot_x, &hotspot_y);
+    /* check for failure or success. */
+    switch (rc) {
+        case BitmapOpenFailed:
+            fprintf(stderr, "XReadBitmapFile - could not open file %s.\n", &filename[0]);
+            break;
+        case BitmapFileInvalid:
+            fprintf(stderr,
+                    "XReadBitmapFile - file '%s' doesn't contain a valid bitmap.\n",
+                    &filename[0]);
+            break;
+        case BitmapNoMemory:
+            fprintf(stderr, "XReadBitmapFile - not enough memory.\n");
+            break;
+        case BitmapSuccess:
+            /* bitmap loaded successfully - do something with it... */
+            BD.P = bitmap;
+            BD.height = bitmap_height;
+            BD.width = bitmap_width;
+            return BD;
+            break;
+    }
+    return BD;
+}
+
+XTextItem makeXTextItem(std::string text, uint32_t X, uint32_t Y)
+{
+    XTextItem XTxt;
+    XTxt.chars = &text[0];
+    XTxt.nchars = text.size();
+    XTxt.delta = 1;
+    return XTxt;
 }
--- src/FWindow.h
+++ src/FWindow.h
@@ -3,10 +3,18 @@
 
 #include <cstdint>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include "FElement.h"
 
+
+struct BitmapData {
+    Pixmap P;
+    uint32_t width;
+    uint32_t height;
+};
 class FWindow : public FElement
 {
 public:
@@ -14,6 +22,7 @@ public:
     void init_server();
     void init_window();
     void open_window();
+    BitmapData createBitmap(std::string filename);
 
     void set_window_title(char* title);
     XRectangle create_rectangle(int16_t x, int16_t y, uint16_t width, uint16_t height);
